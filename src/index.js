8c688c65dae656f1fd16e803b1045759d16b5fce
nishant007-tech <nishanti254710@gmail.com>
2021-08-25 14:05:57 +0530

    designed landing page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/875fffdfa62169a0f131e90f37f1faf4.woff'
 
 ReactDOM.render(
   <React.StrictMode>
