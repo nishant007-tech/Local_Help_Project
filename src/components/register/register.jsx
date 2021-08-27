b2b2e7084b7cba64233973bfac96973fe9adb54e
nishant007-tech <nishanti254710@gmail.com>
2021-08-27 15:17:20 +0530

    dropdown the select category

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,17 +1,67 @@
-import { Button } from '@material-ui/core'
-import React from 'react'
+import { Button, FormHelperText } from '@material-ui/core'
+import React, { useState } from 'react'
 import './register.css'
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import { useRef } from 'react';
 
 function Register() {
+
+    let ulDropDown = useRef();
+
+    let data = [,
+        {
+            category: "Category1"
+        },
+        {
+            category: "Category2"
+        },
+        {
+            category: "Category3"
+        },
+    ]
+    const [category, setCategory] = useState("");
+
+    const handleChange = (event) => {
+        setCategory(event.target.innerText);
+        ulDropDown.current.style.opacity = "0";
+        ulDropDown.current.style.visibility = "hidden";
+    };
+    const handleClick = (e) => {
+        e.stopPropagation();
+        if (ulDropDown.current.style.opacity === "1") {
+            ulDropDown.current.style.opacity = "0";
+            ulDropDown.current.style.visibility = "hidden";
+        } else {
+            ulDropDown.current.style.opacity = "1";
+            ulDropDown.current.style.visibility = "visible";
+        }
+    }
+
+    const outsideClick = (e) => {
+        ulDropDown.current.style.opacity = "0";
+        ulDropDown.current.style.visibility = "hidden";
+    }
+
     return (
-        <div className="registerContainer">
+        <div onClick={outsideClick} className="registerContainer">
             <h3><i className="fas fa-user-circle"></i> Register as a Service Provider.</h3>
             <div className="registerContent">
                 <div className="sectionOne">
                     <label>Full Name</label>
                     <input type="text" placeholder="Enter your full name" />
-                    <label>Category</label>
-                    <input type="text" placeholder="Enter your category" />
+                    <label htmlFor="demo-simple-select">Category</label>
+                    <input value={category} style={{ cursor: "pointer" }} type="text" onClick={handleClick} placeholder="Select Category" />
+                    <ul ref={ulDropDown} className="sub-menu" >
+                        {
+                            data.map(item => (
+                                <li onClick={handleChange} key={item.category} className="sub-menu--item">
+                                    {item.category}
+                                </li>
+                            ))
+                        }
+                    </ul>
                     <label>Contact Number</label>
                     <input type="number" placeholder="Enter your contact number" />
                     <label>Address</label>
@@ -29,7 +79,7 @@ function Register() {
             <div className="RegisterBtn">
                 <Button startIcon={<i className="fas fa-user"></i>} variant="contained" style={{ color: "#fff", backgroundColor: "#FF7139", height: "45px" }}>Register</Button>
             </div>
-        </div>
+        </div >
     )
 }
 
