9bf483d534aa9f900b1ba4f07a8cea8f806e423e
nishant007-tech <nishanti254710@gmail.com>
2021-09-14 23:27:27 +0530

    registration api integrated

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,32 +1,43 @@
 import { Button, FormHelperText } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './register.css'
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
 import { useRef } from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
-
+import { API_URL } from '../../config';
+import axios from 'axios';
 
 function Register() {
 
     let ulDropDown = useRef();
 
-    let data = [,
-        {
-            category: "Category1"
-        },
-        {
-            category: "Category2"
-        },
-        {
-            category: "Category3"
-        },
-    ]
-    const [category, setCategory] = useState("");
+    const [fetchedCategories, setfetchedCategories] = useState([]);
 
-    const handleChange = (event) => {
-        setCategory(event.target.innerText);
+    const [state, setState] = useState({
+        firstName: "",
+        lastName: "",
+        category: "",
+        categoryName: "",
+        gender: "",
+        phoneNo: "",
+        address: "",
+        email: "",
+        profilePic: "",
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let res = await axios.get(`${API_URL}/category`);
+            if (res)
+                setfetchedCategories(res.data?.result);
+        }
+        fetchData();
+    }, [])
+
+    const handleChange = (id, name) => {
+        setState({
+            ...state,
+            category: id,
+            categoryName: name,
+        });
         ulDropDown.current.style.opacity = "0";
         ulDropDown.current.style.visibility = "hidden";
     };
@@ -46,40 +57,80 @@ function Register() {
         ulDropDown.current.style.visibility = "hidden";
     }
 
+    const handleInputChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleFile = async (e) => {
+        let formData = new FormData();
+        formData.append("file", e.target.files[0]);
+        let res = await axios.post(`${API_URL}/service-provider/file-upload`, formData)
+        if (res)
+            setState({
+                ...state,
+                [e.target.name]: res.data?.result?.file
+            })
+    }
+    const handelSubmit = async (e) => {
+        e.preventDefault();
+        let res = await axios.post(`${API_URL}/service-provider/onboard`, {
+            firstName: state.firstName,
+            lastName: state.lastName,
+            gender: state.gender,
+            phoneNo: state.phoneNo,
+            category: state.category,
+            address: state.address,
+            email: state.email,
+            image: state.profilePic,
+        })
+        if (res) {
+            window.location.reload();
+        }
+    }
+
+
+    console.log(state)
+
     return (
         <div onClick={outsideClick} className="registerContainer">
             <h3><i className="fas fa-user-circle"></i> Register as a Service Provider.</h3>
             <div className="registerContent">
                 <div className="sectionOne">
-                    <label>Full Name</label>
-                    <input type="text" placeholder="Enter your full name" />
+                    <label>First Name</label>
+                    <input name="firstName" onChange={handleInputChange} type="text" placeholder="Enter your first name" />
+                    <label>Email</label>
+                    <input name="email" onChange={handleInputChange} type="email" placeholder="Enter your email" />
                     <label htmlFor="demo-simple-select">Category</label>
-                    <input value={category} style={{ cursor: "pointer" }} type="text" onClick={handleClick} placeholder="Select Category" />
+                    <input value={state.categoryName} style={{ cursor: "pointer" }} type="text" onClick={handleClick} placeholder="Select Category" />
                     <ul ref={ulDropDown} className="sub-menu" >
                         {
-                            data.map(item => (
-                                <li onClick={handleChange} key={item.category} className="sub-menu--item">
-                                    {item.category}
+                            fetchedCategories.map(item => (
+                                <li onClick={() => handleChange(item._id, item.name)} key={item.name} className="sub-menu--item">
+                                    {item.name}
                                 </li>
                             ))
                         }
                     </ul>
-                    <label>Contact Number</label>
-                    <input type="number" placeholder="Enter your contact number" />
-                    <label>Address</label>
-                    <input type="text" placeholder="Enter your permanent address" />
+                    <label>Gender</label>
+                    <input name="gender" onChange={handleInputChange} type="text" placeholder="Enter your gender" />
+
                 </div>
                 <div className="sectionTwo">
+                    <label>Last Name</label>
+                    <input name="lastName" onChange={handleInputChange} type="text" placeholder="Enter your last name" />
                     <label>Profile picture</label>
-                    <input type="file" placeholder="Enter your permanent address" />
-                    <label>Rates per hour</label>
-                    <input type="number" placeholder="Enter your rates per hour" />
-                    <label>Work history</label>
-                    <input type="text" placeholder="Enter your work history" />
+                    <input name="profilePic" onChange={handleFile} type="file" placeholder="Enter your permanent address" />
+                    <label>Address</label>
+                    <input name="address" onChange={handleInputChange} type="text" placeholder="Enter your permanent address" />
+                    <label>Phone Number</label>
+                    <input name="phoneNo" onChange={handleInputChange} type="number" placeholder="Enter your phone no." />
                 </div>
             </div>
             <div className="RegisterBtn">
-                <Button startIcon={<i className="fas fa-user"></i>} variant="contained" style={{ color: "#fff", backgroundColor: "#FF7139", height: "45px" }}>Register</Button>
+                <Button onClick={handelSubmit} startIcon={<i className="fas fa-user"></i>} variant="contained" style={{ color: "#fff", backgroundColor: "#FF7139", height: "45px" }}>Register</Button>
             </div>
         </div >
     )
